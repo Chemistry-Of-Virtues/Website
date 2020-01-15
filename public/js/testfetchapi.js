7f624d4547e049ae478a42ef2e054fb3483f594e
wm96n2c <wm96n2c@gmail.com>
2020-01-15 15:44:24 -0700

    Client get and post questions

--- public/js/testfetchapi.js
+++ public/js/testfetchapi.js
@@ -1,1 +1,34 @@
-// Fetch from url /iap/demo/result
+const getQuestionsURL = '/iap/demo'
+const getResultsURL = '/iap/demo/result'
+const questions = []
+
+const postQuestions = async (data) => {
+    fetch(getResultsURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then((res) => {
+        return res.json()
+    }).then((json) => {
+        console.log(json)
+    })
+}
+
+const getQuestions = async () => {
+    await fetch(getQuestionsURL).then((res) => {
+        return res.json()
+    }).then((json) => {
+        json.forEach((question) => {
+            questions.push(question)
+        })
+        return
+    })
+    // Code to use questions here
+
+    // Call to post questions when answers are retreived from the user
+    postQuestions(questions)
+}
+
+getQuestions()
