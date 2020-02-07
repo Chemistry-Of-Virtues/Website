4ad8831a171f90786e8f7aa7ca7be37ce47668d1
wm96n2c <wm96n2c@gmail.com>
2020-02-07 16:44:13 -0700

    All attributes are now displayed.

---
+++ src/utils/traits.js
@@ -0,0 +1,179 @@
+
+const traits = {
+    'Openness To Experience': {
+        'category': 'Big Five Personality Traits',
+        'positive': 'Curious',
+        'negative': 'Cautious',
+        'value': 0
+    },
+    'Conscientiousness': {
+        'category': 'Big Five Personality Traits',
+        'positive': 'Careless',
+        'negative': 'Organized',
+        'value': 0
+    },
+    'Extraversion': {
+        'category': 'Big Five Personality Traits',
+        'positive': 'Outgoing',
+        'negative': 'Reserved',
+        'value': 0
+    },
+    'Agreeableness': {
+        'category': 'Big Five Personality Traits',
+        'positive': 'Friendly',
+        'negative': 'Detached',
+        'value': 0
+    },
+    'Neuroticism': {
+        'category': 'Big Five Personality Traits',
+        'positive': 'Secure',
+        'negative': 'Sensitive',
+        'value': 0
+    },
+    'Mind': {
+        'category': 'Personality Aspects',
+        'positive': 'Internal',
+        'negative': 'External',
+        'value': 0
+    },
+    'Energy': {
+        'category': 'Personality Aspects',
+        'positive': 'Acute',
+        'negative': 'Reasoning',
+        'value': 0
+    },
+    'Nature': {
+        'category': 'Personality Aspects',
+        'positive': 'Intellectual',
+        'negative': 'Moral',
+        'value': 0
+    },
+    'Tactics': {
+        'category': 'Personality Aspects',
+        'positive': 'Decisive',
+        'negative': 'Adaptive',
+        'value': 0
+    },
+    'Abusive': {
+        'category': 'Issues and Behaviors',
+        'positive': 'Awareness/Professional Help/Finding Purpose',
+        'negative': 'Emotional and/or Physical',
+        'value': 0
+    },
+    'Addiction': {
+        'category': 'Issues and Behaviors',
+        'positive': 'Use Purpose and Virtue to Overcome',
+        'negative': 'Substance or Behavioral',
+        'value': 0
+    },
+    'Depression': {
+        'category': 'Issues and Behaviors',
+        'positive': 'Awareness/Professional Help/Finding Purpose',
+        'negative': 'Chemical Imbalance & Situational',
+        'value': 0
+    },
+    'Anger': {
+        'category': 'Issues and Behaviors',
+        'positive': 'Anger Towards Self & Others',
+        'negative': 'Awareness and Conscious Choice of Action',
+        'value': 0
+    },
+    'Positive Detachment': {
+        'category': 'Issues and Behaviors',
+        'positive': 'Mental Block Out/Physical Separation',
+        'negative': 'Passive Agressive',
+        'value': 0
+    },
+    'Negative Detachment': {
+        'category': 'Issues and Behaviors',
+        'positive': 'Engaging in Positive Communication',
+        'negative': 'Mental Withdrawl/Self Medicate/Physical Isolation',
+        'value': 0
+    },
+    'Communication': {
+        'category': 'Issues and Behaviors',
+        'positive': 'Articulate Feelings and Thoughts Calmly',
+        'negative': 'Loud & Argumentative OR Quiet & Withdrawn',
+        'value': 0
+    },
+    'Abused/controlled': {
+        'category': 'Wounds',
+        'positive': 'Positive Detachment',
+        'negative': 'Submissive or Continuing Abuse Cycle',
+        'value': 0
+    },
+    'Abandoned/isolated': {
+        'category': 'Wounds',
+        'positive': 'Vulnerability & Authenticity',
+        'negative': 'Needy/Clingy/Codependant/Negative Detachment',
+        'value': 0
+    },
+    'Addictions Of Others': {
+        'category': 'Wounds',
+        'positive': 'Articulate Feelings and Thoughts Calmly',
+        'negative': 'Loud & Argumentative OR Quiet & Withdrawn',
+        'value': 0
+    },
+    'Under Authority': {
+        'category': 'Wounds',
+        'positive': 'Resilience Through Authenticity/Vulnerability',
+        'negative': 'Trust/Boundary & Self Image Issues, Confrontational',
+        'value': 0
+    },
+    'Non-controlable Circumstances': {
+        'category': 'Wounds',
+        'positive': 'Self Compassion/Adaptive',
+        'negative': 'Hostile, Bitter, Contentious, Self Harming, Withdrawn, Worrying',
+        'value': 0
+    },
+    'Self Compassion': {
+        'category': 'Self-Care',
+        'positive': 'Kindness',
+        'negative': 'Indifference',
+        'value': 0
+    },
+    'Self Control': {
+        'category': 'Self-Care',
+        'positive': 'Moderation',
+        'negative': 'Indulgence',
+        'value': 0
+    },
+    'Integrity': {
+        'category': 'Self-Care',
+        'positive': 'Authenticity',
+        'negative': 'Deceit',
+        'value': 0
+    },
+    'Awareness': {
+        'category': 'Self-Care',
+        'positive': 'Mindfullness',
+        'negative': 'Ignorance',
+        'value': 0
+    },
+    'Intelectual Virtue': {
+        'category': 'Virtue',
+        'positive': 'Submissive/Door Mat',
+        'negative': 'Confrontational/Defensive/Blocked Off',
+        'value': 0
+    },
+    'Moral Virtue': {
+        'category': 'Virtue',
+        'positive': 'Submissive/Door Mat',
+        'negative': 'Confrontational/Defensive/Blocked Off',
+        'value': 0
+    },
+    'Emotional Capacity': {
+        'category': 'Capacity',
+        'positive': 'Submissive/Door Mat',
+        'negative': 'Confrontational/Defensive/Blocked Off',
+        'value': 0
+    },
+    'Mental Capacity': {
+        'category': 'Capacity',
+        'positive': 'Submissive/Door Mat',
+        'negative': 'Confrontational/Defensive/Blocked Off',
+        'value': 0
+    },
+}
+
+module.exports = traits
